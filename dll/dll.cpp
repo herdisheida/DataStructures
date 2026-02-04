3fd417fa011a9afaab9399175c6048732b2526b2
Herdis Heida <herdisj24@ru.is>
2026-02-04 09:49:18 +0000

    template for dll

---
+++ dll/dll.cpp
@@ -0,0 +1,53 @@
+#include <cassert>
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::ws;
+
+
+template <typename Node>
+struct DoublyLinkedList {
+    Node data;
+    Node *prev;
+    Node *next;
+
+
+
+    void sentinel_end_node() {
+
+    }
+
+    void begin_node() {
+
+    }
+    
+    void insert() {
+
+    }
+
+   void erase() {
+
+    }
+
+   void successor() {
+
+    }
+
+   void predecessor() {
+
+    }
+
+    void size() {
+
+    }
+};
+
+
+
+
+int main() {
+
+    
+
+}

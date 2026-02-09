97aa518c602fde5e2375bbb32760af475a436512
Herdis Heida <herdisj24@ru.is>
2026-02-09 11:58:37 +0000

    destructor, assign operator and copy constructor func as well as size func

--- dynamic_array/dynamic_array.h
+++ dynamic_array/dynamic_array.h
@@ -4,39 +4,62 @@
 #include <cstddef>
 
 using std::size_t;
-
+    
 
 template<typename T>
 struct DynamicArray {
-    list = new List<T>;
-    size_t sz;
+    T* data;
+    size_t sz;   // elem count used
+    size_t cap;  // capacity (space in array)
+
+
+    // initialize an empty instance
+    void init_empty(size_t initial_cap) {
+        sz = 0;
+        cap = (initial_cap <= 0 ? 1 : initial_cap);
+        data = new T[cap];
+    }
+
+    // delete all elem, but keep the capacity
+    void clear() {
+        delete[] data;
+        data = 0;
+        sz = 0;
+        cap = 0;
+    }
+
+    // copy from other instance (deep copy)
+    void copy_from(const DynamicArray& other) {
+        sz = other.sz;
+        cap = other.cap;
+        data = new T[cap];
+        for (size_t i = 0; i < sz; i++) data[i] = other.data[i];
+    }
 
     // default constructor
-    DynamicArray() : list(0), sz(0) {
-
-
+    DynamicArray() : data(0), sz(0), cap(0) {
+        init_empty(4);
     }
 
     // auto destructor
     ~DynamicArray() {
-
+        clear();
     }
 
     // copy constructor (deep copy)
-    DynamicArray(const DynamicArray& other) : list(0), sz(0) {
-
+    DynamicArray(const DynamicArray& other) : data(0), sz(0), cap(0) {
+        copy_from(other);
     }
 
     // assignment (operator=) (deep copy)
     DynamicArray& operator=(const DynamicArray& other) {
-
+        if (this != &other) {
+            clear();
+            copy_from(other);
+        }
+        return *this;
     }
 
-    // remove and delete all elem
-    void clear() {
-
-        sz = 0;
-    }
 
     // return the size of the instance
     size_t size() const {

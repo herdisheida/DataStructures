23757376dbecc68ba0c567e311d8734e6c0bcbe6
Herdis Heida <herdisj24@ru.is>
2026-02-09 14:14:10 +0000

    tempalte for structure of has table
    nodes and buckts

--- hash_table/hash_table.h
+++ hash_table/hash_table.h
@@ -6,12 +6,22 @@
 
 using std::cout;
 using std::size_t;
-
     
 
 template<typename T>
 struct HashTable {
+    struct Node {
+        T key;
+        T value;
+        Node* next;
+        Node(T k, T v, Node* n) : key(k), value(v), next(n) {}
+    };
 
+    Node** buckets;
+    size_t bucket_count;
+    size_t sz;
+
+    
 };
 
 

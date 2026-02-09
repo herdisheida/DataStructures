b5b72e51e2308a40d70bafa13a38b39accae0ee5
Herdis Heida <herdisj24@ru.is>
2026-02-09 18:21:27 +0000

    main flow

--- hash_table/main.cpp
+++ hash_table/main.cpp
@@ -5,19 +5,21 @@
 using std::cout;
 using std::cin;
 using std::ws;
+using std::endl;
+
 
 typedef HashTable<int> your_table_type;
 
-your_table_type vecs[1000];
+your_table_type tables[1000];
 
 int main() {
     int q;
     cin >> q;
 
     for (int i = 0; i < q; i++) {
-        int instance;
-        cin >> instance;
-        instance--; // change to 0-based index
+        int id;
+        cin >> id;
+        id--; // change to 0-based index
         // consume whitespace before reading character, otherwise we get a space
         cin >> ws;
         char op;
@@ -28,37 +30,47 @@ int main() {
             cin >> other;
             other--; // change to 0-based index
             // assignment
-
+            tables[id] = tables[other];
         }
         else if (op == 'i') {
             size_t index;
             int value;
             cin >> index >> value;
             // insert
+            tables[id].insert(k, v);
         }
         else if (op == 'e') {
             size_t index;
             cin >> index;
             // erase
+            tables[id].erase(k);
+
         }
         else if (op == 'g') {
             int index;
             cin >> index;
             // get
+            const HashTable::Node* n = tables[id].find(k);
+            if (n) cout << n-> value << endl;
+            else cout << "-" << endl;
+
         }
         else if (op == 's') {
             size_t index;
             int value;
             cin >> index >> value;
             // set
-
+            tables[id][k] = v;
         }
 
         else if (op == 'z') {
-            // print
+            // size
+            cout << tables[id].size() << endl;
         }
         else {
             assert(false);
         }
     }
+
+    return 0;
 }

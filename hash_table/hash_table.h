1a27c74aabbfbbaf275cffb2c241be5b1196f658
Herdis Heida <herdisj24@ru.is>
2026-02-09 18:42:54 +0000

    oparetor []

--- hash_table/hash_table.h
+++ hash_table/hash_table.h
@@ -117,24 +117,30 @@ struct HashTable {
     }
 
     // insert a key-value pair to the hash table. If they key is present, then nothing is inserted
-    // insert()
     void insert(int key, const T& value) {
 
     }
 
     // remove the given key from the hash table
-    // erase()
     void erase(int key) {
 
     }
 
     // provide access to the value associated with a given key
-    // operator[]
     T& operator[](int key) {
+        Node* n = find_node(key);
+
+        // return value
+        if (n) return n -> value;
+
+        // set new value
+        size_t idx = get_bucket_index(key);
+        buckets[idx] = new Node(key, T(), buckets[idx]);
+        sz++;
+        return buckets[idx] -> value;
     }    
 
     // provide the size of the hash table
-    // size()
     size_t size() const {
         return sz;
     }

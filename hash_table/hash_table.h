655670566c2f362516bd45ca556b074482666f03
Herdis Heida <herdisj24@ru.is>
2026-02-09 14:08:07 +0000

    hashttable tempalte connectino to h file etc

---
+++ hash_table/hash_table.h
@@ -0,0 +1,18 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <iostream>
+#include <cstddef>
+
+using std::cout;
+using std::size_t;
+
+    
+
+template<typename T>
+struct HashTable {
+
+};
+
+
+#endif

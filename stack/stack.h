100912cd3c680acdb4461f62650af908917ec195
Herdis Heida <herdisj24@ru.is>
2026-02-11 23:27:46 +0000

    fix stack so that it's similar to heap
    func called reseve instead of resize

--- stack/stack.h
+++ stack/stack.h
@@ -35,18 +35,20 @@ struct Stack {
         for (size_t i = 0; i < sz; i++) data[i] = other.data[i];
     }
 
-    // resize if necessary, otherwise does nothing (for push)
-    void resize() {
-        if (sz == cap) {
-            // double capacity and copy old items to new data array
-            size_t new_cap = cap * 2;
-            T* new_data = new T[new_cap];
-            for (size_t i = 0; i < sz; i++) new_data[i] = data[i];
+    // ensures there is at least the given amount of slots for values
+    void reserve(size_t new_cap) {
+        if (new_cap <= cap) return;
 
-            delete[] data;
-            data = new_data;
-            cap = new_cap;
+        // copy old data to new data 
+        T* new_data = new T[new_cap];
+        for (size_t i = 0; i < sz; i++) {
+            new_data[i] = data[i];
         }
+
+        // delete old and set new values
+        delete[] data;
+        data = new_data;
+        cap = new_cap;
     }
 
     // default constructor
@@ -75,7 +77,7 @@ struct Stack {
 
     // insert an element on top of the stack.
     void push(const T& value) {
-        resize();
+        if (sz == cap) reserve(cap * 2);
         data[sz] = value;
         sz++;
     }

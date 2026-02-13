a81fc2cad1005ee2bc5f072601dd6697d502b6d9
Herdis Heida <herdisj24@ru.is>
2026-02-13 13:23:06 +0000

    add head (first elem) var til að ger styttra time complexity

--- deque/deque.h
+++ deque/deque.h
@@ -11,12 +11,20 @@ struct Deque {
     T* data;
     size_t sz;
     size_t cap;
+    size_t head; // index of first elem
+
+
+    // return the index of the x-th elem
+    size_t idx(size_t x) const {
+        return (head + x) % cap;
+    }
 
     // initialize an empty instance
     void init(size_t initial_cap) {
         sz = 0;
         cap = (initial_cap == 0 ? 1 : initial_cap);
         data = new T[cap];
+        head = 0;
     }
 
     // clear the instance and free memory
@@ -25,14 +33,16 @@ struct Deque {
         data = 0;
         sz = 0;
         cap = 0;
+        head = 0;
     }
 
     // copy from other instance (deep copy)
     void copy_from(const Deque& other) {
         sz = other.sz;
         cap = other.cap;
+        head = 0;
         data = new T[cap];
-        for (size_t i = 0; i < sz; i++) data[i] = other.data[i];
+        for (size_t i = 0; i < sz; i++) data[i] = other.data[other.idx(i)];
     }
 
     // ensures there is at least the given amount of slots for values
@@ -42,29 +52,22 @@ struct Deque {
         // copy old data to new data 
         T* new_data = new T[new_cap];
         for (size_t i = 0; i < sz; i++) {
-            new_data[i] = data[i];
+            new_data[i] = data[idx(i)];
         }
 
         // delete old and set new values
         delete[] data;
         data = new_data;
         cap = new_cap;
+        head = 0;
     }
 
     // default constructor
-    Deque() : data(0), sz(0), cap(0) {
-        init(4);
-    }
-
+    Deque() : data(0), sz(0), cap(0) { init(4); }
     // auto destructor
-    ~Deque() {
-        destroy();
-    }
-
+    ~Deque() { destroy(); }
     // copy constructor
-    Deque(const Deque& other) : data(0), sz(0), cap(0){
-        copy_from(other);
-    }
+    Deque(const Deque& other) : data(0), sz(0), cap(0){ copy_from(other); }
     
     // assignment (operator=)
     Deque& operator=(const Deque& other) {
@@ -75,35 +78,27 @@ struct Deque {
         return *this;
     }
 
-    // insert an elem at the given index
-    void insert(size_t index, const T& value) {
-        if (sz == cap) reserve(cap * 2);
-
-        // create an empty slot to place inserted value
-        for (size_t i = sz; i > index; i--) {
-            data[i] = data[i - 1];
-        }
-        data[index] = value;
-        sz++;
-    }
-
     // insert an element to the front of the deque
     void push_front(const T& value) {
-        insert(0, value);
+        if (sz == cap) reserve(cap * 2);
+        head = (head == 0 ? cap - 1 : head - 1);
+        data[head] = value;
+        sz++;
     }
 
     // insert an element to the back of the deque
     void push_back(const T& value) {
-        insert(sz, value);
+        if (sz == cap) reserve(cap * 2);
+        data[idx(sz)] = value;
+        sz++;
     }
 
     // remove the front element off of the deque
     void pop_front() {
-        if (sz == 0) return;
-        for (size_t i = 1; i < sz; i++) {
-            data[i - 1] = data[i];
+        if (sz > 0) {
+            head = (head + 1) % cap;
+            sz--;
         }
-        sz--;
     }
 
     // remove the back element off of the deque
@@ -112,16 +107,14 @@ struct Deque {
     }
 
     // provide access to the front element of the deque
-    T& front() const { return data[0]; }
-
+    const T& front() const { return data[head]; }
     // provide access to the back element of the deque
-    T& back() const { return data[sz - 1]; }
+    const T& back() const { return data[idx(sz - 1)]; }
 
     // provide the size of the Deque
     size_t size() const {
         return sz;
     }
-
 };
 
 #endif
